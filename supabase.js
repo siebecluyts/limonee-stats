139783978d7e83af27ce53c6bec5b724a03bd0ce
SiebeCluyts <debendevanzelem1@gmail.com>
2025-06-30 14:10:48 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+// supabase.js
+const SUPABASE_URL = 'https:///vyesjdndzshbiweslzpx.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5ZXNqZG5kenNoYml3ZXNsenB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4NzQ2NTUsImV4cCI6MjA2NjQ1MDY1NX0.fsMkTKVcCH-WqPaLuwg1dxOgBekDz11Jd4Htf1FVGrs';
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
